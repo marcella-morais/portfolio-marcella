50d558fa6fece964cfb06ef248fb5a1229977c28
marcella-morais <116105213+marcella-morais@users.noreply.github.com>
2024-06-26 13:38:04 -0300

    feat: navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-
+import React from 'react';
+import Navbar from './components/Navbar/Navbar';
 
 const App = () => {
-
   return (
     <>
-      App
+      <Navbar />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react'
+import './Navbar.css'
+import MobileNav from './MobileNav/MobileNav'
+
+const Navbar = () => {
+
+    const [openMenu, setOpenMenu] = useState(false);
+
+    const toggleMenu = () => {
+        setOpenMenu(!openMenu);
+    };
+
+    return (
+        <>
+        <MobileNav isOpen={openMenu} toggleMenu={toggleMenu} />
+
+        <nav className="nav-wrapper">
+            <div className="nav-content">
+                <img className="logo" src="../public/assets/images/logo.png" alt="" />
+
+                <ul>
+                    <li>
+                        <a className="menu-item" href="">Home</a>
+                    </li>
+                    <li>
+                        <a className="menu-item" href="">Skills</a>
+                    </li>
+                    <li>
+                        <a className="menu-item" href="">Work Experience</a>
+                    </li>
+                    <li>
+                        <a className="menu-item" href="">Contact Me</a>
+                    </li>
+
+                    <button className="contact-btn" onClick={() => {}}>
+                        Hire Me
+                    </button>
+                </ul>
+
+                <button className="menu-btn" onClick={toggleMenu}>
+                    <span 
+                        className={"material-symbols-outlined"}
+                        style={{ fontSize: "1.8rem"}}
+                        >
+                        {openMenu ? "x" : "-"}        
+                    </span>
+                </button>
+
+            </div>
+        </nav>
+        </>
+    )
+}
+
+export default Navbar
